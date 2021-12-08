823425c1e522f267e52a7e9a5de43c5a5a0dd3a8
Irmantas.Č <67824760+Likvidate@users.noreply.github.com>
2021-12-08 11:45:25 +0200

    Add files via upload

---
+++ VuciFormItemRadio.vue
@@ -0,0 +1,34 @@
+<template>
+  <vuci-form-item-template v-bind="VuciFormItemTemplateProps">
+    <a-radio-group v-model="model" @change="onChange">
+      <component :is="is" v-for="(value, key) in options" :key=key :value=value >{{value}}</component>
+    </a-radio-group>
+  </vuci-form-item-template>
+</template>
+
+<script>
+import VuciFormItemMixin from './VuciFormItemMixin'
+export default {
+  name: 'VuciRadio',
+  mixins: [VuciFormItemMixin],
+  props: {
+    isButton: Boolean,
+    options: Array
+  },
+  data: function () {
+    return {
+    }
+  },
+  computed: {
+    is () {
+      if (this.isButton) return 'a-radio-button'
+      return 'a-radio'
+    }
+  },
+  methods: {
+    onChange (e) {
+      console.log('radio checked', e.target.value)
+    }
+  }
+}
+</script>
